26911192e37cc7ec916986a166e0303592f8fef4
RaphaSuryaJi <rapha.suryaji@gmail.com>
2024-06-29 14:31:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Manifesto Esmeralda carregado!');
-    
+
+    const acc = document.querySelectorAll('.accordion');
+    acc.forEach(button => {
+        button.addEventListener('click', function () {
+            this.classList.toggle('active');
+            const panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    });
+
     const joinButton = document.getElementById('joinUs');
     joinButton.addEventListener('click', () => {
         alert('Obrigado por se juntar a nós na jornada para um futuro regenerativo e consciente!');
